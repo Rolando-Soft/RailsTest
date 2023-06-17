4662d7f82708281df24ca5691d23ce977135c183
Rolando-Soft <59375585+Rolando-Soft@users.noreply.github.com>
2023-06-17 00:49:25 -0400

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Equipment.create(serial: "RRQA2", techid: "4460")
+Equipment.create(serial: "RRQA4", techid: "7780")
+Equipment.create(serial: "RRQA7", techid: "1460")
+Equipment.create(serial: "RRQA6", techid: "0720")
